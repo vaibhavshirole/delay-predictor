1b119aa5cc548f823abc0fb6acd61889c4488392
shiro019 <shiro019@umn.edu>
2023-11-03 17:13:18 +0800

    grid based gui

--- gui/playground.py
+++ gui/playground.py
@@ -4,20 +4,33 @@ root = tk.Tk()
 root.title("Flight Delay Predictor")
 root.geometry("600x400")
 
-flightNum = tk.Entry(root, width=50)
-flightNum.pack()
-flightNum.insert(0, "Enter Flight Number: ")
+num_var = tk.StringVar()
+date_var = tk.StringVar() 
 
-flightDate = tk.Entry(root, width=50)
-flightDate.pack()
-flightDate.insert(0, "Enter Date of Flight: ")
+def submit():
 
-def myClick():
-    hello = "Hello " + flightNum.get()
-    myLabel = tk.Label(root, text=hello)
-    myLabel.pack()
+    flight_number = num_entry.get()
+    hello = "Selected flight: " + flight_number
 
-myButton = tk.Button(root, text="Submit", command=myClick)
-myButton.pack()
+    output = tk.Label(root, text=hello)
+    output.grid(row=2,column=2)
+
+# creating label and text entry for flight_number
+num_label = tk.Label(root, text = 'Enter Flight Number: ')
+num_entry = tk.Entry(root, textvariable = num_var)
+
+# creating label and date entry for flight_date
+date_label = tk.Label(root, text = 'Enter Flight Date: ')
+date_entry = tk.Entry(root, textvariable = date_var)
+
+# creating a button to call submit func
+sub_btn=tk.Button(root,text = 'ESTIMATE', command = submit, font=('calibre',14, 'bold'))
+
+# placing initial labels and entry boxes on grid
+num_label.grid(row=0, column=0, padx = 5, pady = 10)
+num_entry.grid(row=0, column=1, padx = 5, pady = 10)
+date_label.grid(row=1, column=0, padx = 20, pady = 10)
+date_entry.grid(row=1, column=1, padx = 20, pady = 10)
+sub_btn.grid(row=2, column=1, padx = 20, pady = 10)
 
 root.mainloop()
