deae08d0c1652f862689f70b4766bef1bd8e7a64
shiro019 <shiro019@umn.edu>
2023-11-08 17:11:25 +0800

    reusable label replacement

--- gui/playground.py
+++ gui/playground.py
@@ -19,10 +19,13 @@ def submit():
     # user_date_input = "Selected flight date: " + flight_date
     # chosen_date = tk.Label(root, text=user_date_input)
     # chosen_date.grid(row=3,column=1)
+    global estimated_delay
 
     if(flight_number_entry.get()):
         # display prediction on gui
         # TO-DO: CALL PREDICTION CODE, GET RETURN VALUE AND DISPLAY IT
+        estimated_delay.grid_forget()
+
         delay = "Delay: 100"
         estimated_delay = tk.Label(root, text=delay, font=('calibre',14, 'bold'))
         estimated_delay.grid(row=4, column=0, padx=20, pady=10, sticky="w")
@@ -30,6 +33,10 @@ def submit():
         arrival = "Arrival time: 19:35"
         estimated_arrival = tk.Label(root, text=arrival, font=('calibre',14, 'bold'))
         estimated_arrival.grid(row=5, column=0, padx=20, pady=10, sticky="w")
+    else:
+        delay = "Input valid flight number"
+        estimated_delay = tk.Label(root, text=delay, font=('calibre',14, 'bold'))
+        estimated_delay.grid(row=4, column=1, padx=20, pady=10, sticky="w")
 
 # creating label and text entry for flight_number
 flight_number_label = tk.Label(root, text = 'Enter Flight Number: ')
@@ -38,10 +45,10 @@ flight_number_label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
 flight_number_entry.grid(row=0, column=1, padx=50, pady=10, sticky="e")
 
 # creating label and date entry for flight_date
-date_label = tk.Label(root, text = 'Enter Flight Date: ')
-date_entry = tk.Entry(root, textvariable = flight_date)
-date_label.grid(row=1, column=0, padx=20, pady=10, sticky="w")
-date_entry.grid(row=1, column=1, padx=50, pady=10, sticky='e')
+# date_label = tk.Label(root, text = 'Enter Flight Date: ')
+# date_entry = tk.Entry(root, textvariable = flight_date)
+# date_label.grid(row=1, column=0, padx=20, pady=10, sticky="w")
+# date_entry.grid(row=1, column=1, padx=50, pady=10, sticky='e')
 
 # creating a button to call submit func
 sub_btn=tk.Button(root,text = 'ESTIMATE', command = submit, font=('calibre',14, 'bold'))
